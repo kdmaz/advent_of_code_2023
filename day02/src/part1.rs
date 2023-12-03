dfdd91fb78878040e13a95e3694d0dc28b408dda
Kyle Mazeika <kyle.mazeika@gmail.com>
2023-12-02 22:24:16 -0500

    refactor: day02 part1

--- day02/src/part1.rs
+++ day02/src/part1.rs
@@ -2,72 +2,68 @@ const MAX_RED_CUBES: i32 = 12;
 const MAX_GREEN_CUBES: i32 = 13;
 const MAX_BLUE_CUBES: i32 = 14;
 
-#[derive(Debug)]
-pub struct Game {
-    pub id: i32,
-    pub is_possible: bool,
-}
-
-pub enum Color {
-    Blue(i32),
-    Red(i32),
-    Green(i32),
-}
-
-impl Color {
-    pub fn new(text: &str) -> Self {
-        let mut split = text.trim().split(' ');
-        let num = split.next().unwrap().parse().unwrap();
-        let color = split.next().unwrap();
-
-        match color {
-            "blue" => Self::Blue(num),
-            "red" => Self::Red(num),
-            "green" => Self::Green(num),
-            _ => unreachable!(),
-        }
-    }
-
-    pub fn get_is_possible(&self) -> bool {
-        match self {
-            Color::Blue(n) => *n <= MAX_BLUE_CUBES,
-            Color::Red(n) => *n <= MAX_RED_CUBES,
-            Color::Green(n) => *n <= MAX_GREEN_CUBES,
-        }
-    }
-}
-
-impl Game {
-    pub fn new(line: &str) -> Self {
-        let mut split = line.split(':');
-
-        Self {
-            id: Game::parse_id(split.next().unwrap()),
-            is_possible: Game::get_is_possible(split.next().unwrap()),
-        }
-    }
-
-    fn parse_id(text: &str) -> i32 {
-        text.split(' ').last().unwrap().parse::<i32>().unwrap()
-    }
-
-    fn get_is_possible(rounds: &str) -> bool {
-        rounds.split(';').all(|round| {
-            round
-                .split(',')
-                .map(Color::new)
-                .all(|c| c.get_is_possible())
-        })
-    }
-}
-
 pub fn part1(input: &str) -> i32 {
     input
         .lines()
         .map(Game::new)
-        .filter(|game| game.is_possible)
+        .filter(|game| game.is_valid)
         .map(|game| game.id)
         .sum()
+}
+
+pub struct Game {
+    pub id: i32,
+    pub is_valid: bool,
+}
+
+impl Game {
+    pub fn new(line: &str) -> Self {
+        let mut game_parts = line.split(':');
+
+        Self {
+            id: Game::parse_id(game_parts.next().unwrap()),
+            is_valid: Game::get_is_valid(game_parts.next().unwrap()),
+        }
+    }
+
+    fn parse_id(id_raw: &str) -> i32 {
+        id_raw.split(' ').last().unwrap().parse::<i32>().unwrap()
+    }
+
+    fn get_is_valid(rounds: &str) -> bool {
+        rounds
+            .split(';')
+            .all(|round| round.split(',').map(Color::new).all(|c| c.get_is_valid()))
+    }
+}
+
+pub enum Color {
+    Blue { num_seen: i32 },
+    Red { num_seen: i32 },
+    Green { num_seen: i32 },
+}
+
+impl Color {
+    pub fn new(draw: &str) -> Self {
+        let mut draw_parts = draw.trim().split(' ');
+        let num_seen = draw_parts.next().unwrap().parse().unwrap();
+        let color = draw_parts.next().unwrap();
+
+        match color {
+            "blue" => Self::Blue { num_seen },
+            "red" => Self::Red { num_seen },
+            "green" => Self::Green { num_seen },
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn get_is_valid(&self) -> bool {
+        match self {
+            Color::Blue { num_seen } => *num_seen <= MAX_BLUE_CUBES,
+            Color::Red { num_seen } => *num_seen <= MAX_RED_CUBES,
+            Color::Green { num_seen } => *num_seen <= MAX_GREEN_CUBES,
+        }
+    }
 }
 
 #[cfg(test)]
