ca1aceab8307d7507212fdd48a0c1da8f30eeaeb
Kyle Mazeika <kyle.mazeika@gmail.com>
2023-12-03 15:21:30 -0500

    feat: day03 part2

--- day03/src/part2.rs
+++ day03/src/part2.rs
@@ -1,5 +1,166 @@
-pub fn part2(_input: &str) -> i32 {
-    todo!()
+pub fn part2(input: &str) -> i32 {
+    let rows = input.lines().map(|line| line.chars().collect()).collect();
+    let grid = Grid::new(rows);
+    let mut gear_ratio_total = 0;
+
+    for (r, row) in grid.rows.iter().enumerate() {
+        for (c, &char) in row.iter().enumerate() {
+            if char != '*' {
+                continue;
+            }
+
+            if let Some((n1, n2)) = grid.get_two_adjacent_part_nums(Position {
+                row_index: r,
+                col_index: c,
+            }) {
+                gear_ratio_total += n1.value * n2.value;
+            }
+        }
+    }
+
+    gear_ratio_total
+}
+
+struct Grid {
+    rows: Vec<Vec<char>>,
+}
+
+impl Grid {
+    fn new(rows: Vec<Vec<char>>) -> Self {
+        Grid { rows }
+    }
+
+    fn get_two_adjacent_part_nums(&self, pos: Position) -> Option<(GridNumber, GridNumber)> {
+        let r = pos.row_index as i32;
+        let c = pos.col_index as i32;
+
+        let get_is_new_num = |r: i32, c: i32, nums: &Vec<GridNumber>| {
+            let r = r as usize;
+            let c = c as usize;
+            for &num in nums.iter() {
+                let is_same_row = r == num.row_index;
+                let is_within_num_bounds = c >= num.start_index && c <= num.end_index;
+
+                if is_same_row && is_within_num_bounds {
+                    return false;
+                }
+            }
+
+            true
+        };
+
+        let get_is_digit = |r: i32, c: i32| {
+            let left_out_of_bounds = c < 0;
+            let top_out_of_bounds = r < 0;
+
+            if left_out_of_bounds || top_out_of_bounds {
+                return false;
+            }
+
+            let r = r as usize;
+            let c = c as usize;
+
+            let right_out_of_bounds = c + 1 > self.rows[r].len() - 1;
+            let bottom_out_of_bounds = r + 1 > self.rows.len();
+
+            if right_out_of_bounds || bottom_out_of_bounds {
+                return false;
+            }
+
+            self.rows[r][c].is_ascii_digit()
+        };
+
+        let top_left = (r - 1, c - 1);
+        let top = (r - 1, c);
+        let top_right = (r - 1, c + 1);
+        let right = (r, c + 1);
+        let bottom_right = (r + 1, c + 1);
+        let bottom = (r + 1, c);
+        let bottom_left = (r + 1, c - 1);
+        let left = (r, c - 1);
+
+        let nums = [
+            top_left,
+            top,
+            top_right,
+            right,
+            bottom_right,
+            bottom,
+            bottom_left,
+            left,
+        ]
+        .iter()
+        .fold(Vec::with_capacity(3), |mut nums, (r, c)| {
+            let r = *r;
+            let c = *c;
+            if get_is_digit(r, c) && get_is_new_num(r, c, &nums) {
+                nums.push(self.get_grid_num(Position::new(r as usize, c as usize)));
+            }
+
+            nums
+        });
+
+        if nums.len() == 2 {
+            let mut it = nums.into_iter();
+            Some((it.next().unwrap(), it.next().unwrap()))
+        } else {
+            None
+        }
+    }
+
+    fn get_grid_num(&self, pos: Position) -> GridNumber {
+        let r = pos.row_index;
+        let mut start = pos.col_index;
+        while start > 0 && self.rows[r][start - 1].is_ascii_digit() {
+            start -= 1;
+        }
+
+        let mut end = pos.col_index;
+        while end < self.rows[r].len() - 1 && self.rows[r][end + 1].is_ascii_digit() {
+            end += 1;
+        }
+
+        let mut value = 0;
+        for i in start..=end {
+            value = value * 10 + self.rows[r][i].to_digit(10).unwrap() as i32;
+        }
+
+        GridNumber::new(r, start, end, value)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct GridNumber {
+    value: i32,
+    row_index: usize,
+    start_index: usize,
+    end_index: usize,
+}
+
+impl GridNumber {
+    fn new(row_index: usize, start_index: usize, end_index: usize, value: i32) -> Self {
+        Self {
+            value,
+            row_index,
+            start_index,
+            end_index,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Position {
+    row_index: usize,
+    col_index: usize,
+}
+
+impl Position {
+    fn new(row_index: usize, col_index: usize) -> Self {
+        Position {
+            row_index,
+            col_index,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -10,7 +171,7 @@ mod tests {
     fn part2_example() {
         let input = include_str!("../example.txt");
         let output = part2(input);
-        let expected = 0;
+        let expected = 467835;
         assert_eq!(output, expected);
     }
 
@@ -18,7 +179,7 @@ mod tests {
     fn part2_input() {
         let input = include_str!("../input.txt");
         let output = part2(input);
-        let expected = 0;
+        let expected = 81709807;
         assert_eq!(output, expected);
     }
 }
