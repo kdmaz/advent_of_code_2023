c1f33214869a45abf3fe83d9c695bcebf9d424b9
Kyle Mazeika <kyle.mazeika@gmail.com>
2023-12-04 21:07:04 -0500

    feat: day04 part2

--- day04/src/part2.rs
+++ day04/src/part2.rs
@@ -1,5 +1,50 @@
-pub fn part2(_input: &str) -> i32 {
-    todo!()
+use std::{
+    cell::RefCell,
+    collections::{HashMap, HashSet},
+};
+
+pub fn part2(input: &str) -> i32 {
+    let card_copy_map = RefCell::new(HashMap::new());
+
+    input
+        .lines()
+        .map(|line| {
+            let mut line_parts = line.split(':');
+            let card_num = line_parts
+                .next()
+                .unwrap()
+                .split(' ')
+                .last()
+                .unwrap()
+                .parse::<i32>()
+                .unwrap();
+            let mut card_parts = line_parts.last().unwrap().split('|');
+            let winning_nums = HashSet::<i32>::from_iter(
+                card_parts
+                    .next()
+                    .unwrap()
+                    .split(' ')
+                    .filter_map(|s| s.parse::<i32>().ok()),
+            );
+
+            let copies_won = card_parts
+                .next()
+                .unwrap()
+                .split(' ')
+                .filter_map(|s| s.parse::<i32>().ok())
+                .filter(|n| winning_nums.contains(n))
+                .count() as i32;
+
+            let current_num_copies = card_copy_map.borrow().get(&card_num).copied().unwrap_or(1);
+
+            for i in 1..=copies_won {
+                *card_copy_map.borrow_mut().entry(card_num + i).or_insert(1) += current_num_copies;
+            }
+
+            card_copy_map.borrow_mut().remove(&card_num);
+            current_num_copies
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -10,7 +55,7 @@ mod tests {
     fn part2_example() {
         let input = include_str!("../example.txt");
         let output = part2(input);
-        let expected = 0;
+        let expected = 30;
         assert_eq!(output, expected);
     }
 
@@ -18,7 +63,7 @@ mod tests {
     fn part2_input() {
         let input = include_str!("../input.txt");
         let output = part2(input);
-        let expected = 0;
+        let expected = 13114317;
         assert_eq!(output, expected);
     }
 }
